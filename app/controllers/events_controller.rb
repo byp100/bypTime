5e18c58b2e259dd31d34e6f532dca834c6ea0bfa
Ayanga Okpokowuruk <ayanga.okpokowuruk@manheim.com>
2015-02-12 00:11:57 -0500

    Added actions and links for Events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,20 +1,45 @@
 class EventsController < ApplicationController
-  def new
+  def index
+    @events = Event.all
   end
 
   def show
     @event = Event.find params[:id]
   end
 
-  def index
-    @events = Event.all
+  def new
+    @event = Event.new
+  end
+
+  def edit
+    @event = Event.find params[:id]
   end
 
   def create
     @event = Event.new(event_params)
 
-    @event.save
-    redirect_to @event
+    if @event.save
+      redirect_to @event
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @event = Event.find params[:id]
+
+    if @event.update(event_params)
+      redirect_to @event
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @event = Event.find params[:id]
+    @event.destroy
+
+    redirect_to events_path
   end
 
   private
