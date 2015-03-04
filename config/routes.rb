5eaf20d4cb6ce5af504e4d8a48ffde72f5604a13
Ayanga Okpokowuruk <ayanga.okpokowuruk@manheim.com>
2015-03-03 20:21:06 -0600

    Added link for Profile to header, changed Events index to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :members
   resources :members
-  root 'static_pages#home'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
   resources :events
+  root 'events#index'
 
   devise_scope :member do
     post 'new_attendee' => 'members#create_attendee'
