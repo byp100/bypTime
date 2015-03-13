80b510aa206a8549502eee69179681868d8231cd
Rose Afriyie <rose@SerwahBonsu.home.local>
2015-03-12 21:01:55 -0500

    Add button and navbar and edit create_user table

--- db/migrate/20150217034056_add_fields_to_user.rb
+++ db/migrate/20150217034056_add_fields_to_user.rb
@@ -1,5 +1,8 @@
 class AddFieldsToUser < ActiveRecord::Migration
   def change
+  	create_table :users do |t|
+     t.timestamps null: false
+   end
     add_column :users, :name, :string
     add_column :users, :birthdate, :datetime
     add_column :users, :email, :string
