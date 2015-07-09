1df961d4b07c500292181101528c4c8ea27f0379
Ayanga Okpokowuruk <ayanga.okpokowuruk@manheim.com>
2015-07-08 19:19:15 -0500

    Admin can view event attendees in the dashboard

--- app/admin/event.rb
+++ app/admin/event.rb
@@ -1,18 +1,23 @@
 ActiveAdmin.register Event do
+  permit_params :title, :description, :start_time, :end_time, :location, :address
 
+  show do
+    attributes_table do
+      row :title
+      row :description
+      row :start_time
+      row :end_time
+      row :location
+      row :address
+    end
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
-
-
+    panel 'Attendees' do
+      table_for event.attendances do
+        column 'name' do |attendance|
+          attendance.member.name
+        end
+        column :in_attendance
+      end
+    end
+  end
 end
