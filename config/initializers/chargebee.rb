be520ec3cf59f8ba289790e7a717206c0a6ba625
Rose Afriyie <rose@mrelief.com>
2015-11-20 21:22:55 -0600

    Add feature to ensure member is added to orientation on sign-in

---
+++ config/initializers/chargebee.rb
@@ -0,0 +1,1 @@
+ChargeBee.configure(:site => ENV['chargebee_site'], :api_key => ENV['chargebee_key'])
--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,32 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/about'
 
-  devise_for :users
-  resources :users do
-    post 'check_in'
-  end
-
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  resources :events
-  put 'unattend' => 'events#unattend', as: 'unattend'
+  authenticate :user do 
+    resources :users do
+      user do
+        post 'check_in'
+      end
+    end
 
-  devise_scope :user do
-    post 'create_attendee' => 'users#create_attendee', as: 'create_attendee'
+    resources :events do
+      resources :users do
+      end
+    end
+
+    root 'static_pages#home'
+    put 'unattend' => 'events#unattend', as: 'unattend'
+    put 'check_in' => 'events#check_in', as: 'check_in'
+    put 'undo_check_in' => 'events#undo_check_in', as: 'undo_check_in'
+
+    devise_scope :user do
+      post 'create_attendee' => 'users#create_attendee', as: 'create_attendee'
+    end
   end
 end
+
+
+
+
