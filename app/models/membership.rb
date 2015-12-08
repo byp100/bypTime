540994061e9ead638708105780bbc356c28aba7c
Ellie Day <heyellieday@gmail.com>
2015-12-07 18:30:51 -0600

    Rebased from master and added dashboard

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,7 +1,4 @@
 class Membership < ActiveRecord::Base
 	belongs_to :organization
-	belongs_to :member
-
-
-
+	belongs_to :member, class_name: "User", foreign_key: "user_id"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -63,7 +63,7 @@ class User < ActiveRecord::Base
   end
 
   def eligible_for_membership?
-    if attendances.where(event_type: "orientation").count <= 1 && attendances.where(event_type: "general_body_meeting").count <= 2 && attendances.where(event_type: "public_event").count <= 1
+    if attendances.joins(:events).where("events.event_type = orientation").count <= 1 && attendances.joins(:events).where("events.event_type = general_body_meeting").count <= 2 && attendances.joins(:events).where("events.event_type = public_event").count <= 1
       return true
     else
       return false
