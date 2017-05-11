cdc8f97b2ef191d9286f532fc0b17feb7f862fb1
Ayanga Okpokowuruk <ayanga@aok-solutions.com>
2017-05-11 00:17:27 -0400

    refactoring billing/webhooks controllers and user model

--- app/controllers/billing_controller.rb
+++ app/controllers/billing_controller.rb
@@ -1,98 +1,92 @@
 class BillingController < ApplicationController
   rescue_from ChargeBee::PaymentError do |ex|
-      # First check for card parameters entered by the user.
-      # We recommend you to validate the input at the client side itself to catch simple mistakes.
-      if "card[number]" == ex.param
-        # Ask your user to recheck the card number. A better way is to use
-        # Stripe's https://github.com/stripe/jquery.payment for validating it in the client side itself.
+    # First check for card parameters entered by the user.
+    # We recommend you to validate the input at the client side itself to catch simple mistakes.
+    if "card[number]" == ex.param
+      # Ask your user to recheck the card number. A better way is to use
+      # Stripe's https://github.com/stripe/jquery.payment for validating it in the client side itself.
 
       #elsif <other card params> == ex.param
-        # Similarly check for other card parameters entered by the user.
-        # ....
+      # Similarly check for other card parameters entered by the user.
+      # ....
 
-      else
-        redirect_to :back, notice: "Your payment method was declined. Please try another card."
-      end
-  end  
+    else
+      redirect_to :back, notice: "Your payment method was declined. Please try another card."
+    end
+  end
   rescue_from ChargeBee::InvalidRequestError do |ex|
     redirect_to :back, notice: ex.param
   end
+
   def edit
-  	if current_user.subscribed?
-     result = ChargeBee::Subscription.retrieve(current_user.customer_id)
-      @customer = result.customer
-      result = ChargeBee::Card.retrieve(current_user.customer_id)
-      card = result.card
+    if current_user.subscribed?
+      @customer = ChargeBee::Subscription.retrieve(current_user.customer_id).customer
+      card = ChargeBee::Card.retrieve(current_user.customer_id).card
       @card_summary = "Card Details: #{card.card_type.titleize} ending in #{card.last4}"
     else
-    	redirect_to billing_enroll_path
+      redirect_to billing_enroll_path
     end
   end
 
   def pending_invoices
-    @pending_invoices = Billing::Invoice.list_invoices(current_user.customer_id).select{|result| result.invoice.status == "pending"}
+    @pending_invoices = ChargeBee::Invoice.invoices_for_customer(current_user.customer_id).select {|result| result.invoice.status == "pending"}
   end
 
   def close_invoice
-    Billing::Invoice.collect(params[:invoice_id])
+    ChargeBee::Invoice.collect(params[:invoice_id])
     redirect_to billing_pending_invoices_path, notice: "Thanks for paying your invoice!"
   end
 
   def update_card
-    result = ChargeBee::Card.update_card_for_customer(current_user.customer_id, {
-      tmp_token: params[:stripeToken]
+    ChargeBee::Card.update_card_for_customer(current_user.customer_id, {
+        tmp_token: params[:stripeToken]
     })
     render json: {success: true}
   end
 
   def update_contact
-    result = ChargeBee::Customer.update(current_user.customer_id, params[:customer])
-    result = ChargeBee::Customer.update_billing_info(current_user.customer_id, {
-      billing_address: params[:customer][:billing_address]
+    ChargeBee::Customer.update(current_user.customer_id, params[:customer])
+    ChargeBee::Customer.update_billing_info(current_user.customer_id, {
+        billing_address: params[:customer][:billing_address]
     })
     redirect_to dashboard, notice: "The billing contact info has been updated."
   end
 
   def enroll
-  	if current_user.subscribed?
-  		redirect_to billing_edit_path
-	  else
-	  	plans = ChargeBee::Plan.list.select{|plan| plan.plan.status == "active"}
-	  	@plans = []
-	  	plans.each do |plan|
-	  		@plans.push(plan.plan)
-	  	end
-	  end
+    if current_user.subscribed?
+      redirect_to billing_edit_path
+    else
+      @plans = ChargeBee::Plan.list.select {|plan| plan.plan.status == "active"}.map {|plan| plan.plan}
+    end
 
   end
 
   def overview
-  	if current_user.aasm_state == "active"
-  		redirect_to billing_edit_path
-  	else
-  		redirect_to billing_enroll_path
-  	end
+    if current_user.aasm_state == "active"
+      redirect_to billing_edit_path
+    else
+      redirect_to billing_enroll_path
+    end
   end
 
- def subscribe
-   if current_user.aasm_state == "prospective"
-       customer = params[:subscription][:customer]
-       
-       customer[:billing_address][:first_name] = customer[:first_name]
-       customer[:billing_address][:last_name] = customer[:last_name]
+  def subscribe
+    if current_user.aasm_state == "prospective"
+      customer = params[:subscription][:customer]
 
-       result = ChargeBee::Subscription.create({
-         :plan_id => params[:subscription][:plan_id],
-         :customer => customer,
-         :card => {
-           :tmp_token => params[:subscription][:stripeToken]
-         }
-       })
-       current_user.customer_id = result.customer.id
-       current_user.save
-       current_user.induct!
-   else
-     redirect_to settings_billing_path, notice: "You are already paying dues."
-   end
- end
+      customer[:billing_address][:first_name] = customer[:first_name]
+      customer[:billing_address][:last_name] = customer[:last_name]
+
+      subscription = create_subscription(customer)
+      current_user.update(customer_id: subscription.customer.id)
+    else
+      redirect_to settings_billing_path, notice: "You are already paying dues."
+    end
+  end
+
+  def create_subscription(customer)
+    ChargeBee::Subscription.create({plan_id: params[:subscription][:plan_id],
+                                    customer: customer,
+                                    card: {tmp_token: params[:subscription][:stripeToken]}
+                                   })
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :registerable, recoverable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :rememberable, :trackable, :validatable,
-         :authentication_keys => [:phone]
+         authentication_keys: [:phone]
 
   validates :phone, presence: true, uniqueness: true, format: { with: /\d{10}/ }
   validates :name, presence: true
@@ -26,8 +26,7 @@ class User < ActiveRecord::Base
 
   after_initialize :set_default_role, if: :new_record?
 
-
-  scope :active, -> { where(:aasm_state => "active") }
+  scope :active, -> { where(aasm_state: "active") }
 
 
   def rsvp? event
@@ -43,122 +42,6 @@ class User < ActiveRecord::Base
     self.role ||= :guest
   end
 
-  def self.import(file, organization)
-    file_path = file.tempfile.to_path
-    xlsx = Roo::Excelx.new(file_path)
-    xlsx.default_sheet = xlsx.sheets.first
-
-    row_count = 0
-
-    event_count = 0
-
-    event_data = []
-
-    xlsx.each_row_streaming(pad_cells: true) do |row|
-
-      row_items = row
-
-      if row_count == 0
-
-        row_items.each_with_index do |item, index|
-          item = item.value
-          if index > 3
-            if item.present?
-              event_type = "public_event"
-              if item == "Orientation"
-                event_type = "orientation"
-              elsif item == "Meeting"
-                event_type = "general_body_meeting"
-              end
-              event_data.push({
-                event_type: event_type,
-                attendances: []
-              })
-            else
-              event_data.push({
-              })
-            end
-          end
-
-        end
-
-      elsif row_count == 1
-        row_items.each_with_index do |item, index|
-          item = item.value
-          if index > 3
-            event_data[index - 4][:date] = item
-          end
-        end
-      elsif row_count == 2
-
-        row_items.each_with_index do |item, index|
-          item = item.value
-          if index > 3
-            if item.blank?
-              event_data[index - 4][:title] = "Event"
-            else
-              event_data[index - 4][:title] = item
-            end
-          end
-        end
-
-      elsif row_count >= 3
-        user_data = {}
-        row_items.each_with_index do |item, index|
-          if item.present?
-            item = item.value
-            if index == 0
-              user_data[:name] = item
-            elsif index == 1
-              user_data[:phone] = item
-            elsif index == 3
-              user_data[:email] = item
-            end
-            user = nil
-            if index == 4
-              user = User.find_by(phone: user_data[:phone].to_s) if user_data[:phone].present?
-              if user.nil?
-                user = User.create(name: user_data[:name], phone: user_data[:phone].to_i.to_s, email: user_data[:email], password: "password", password_confirmation: "password")
-                user.memberships.create!(organization: organization) if user.id.present?
-              end
-
-            end
-            if index >= 4
-              event_data[index - 4][:attendances].push(user.id) if item.present? && user.present?
-            end
-            user = nil
-          else
-            if index == 0
-              break
-            end
-          end
-        end
-      end  
-
-      row_count += 1
-    end
-    event_data.each do |data|
-      if data[:event_type].present?
-        datetime = data[:date].to_datetime
-        if Event.find_by(start_time: datetime.beginning_of_day..datetime.end_of_day).nil?
-          event = Event.find_or_create_by!(event_type: data[:event_type].downcase, start_time: data[:date].to_datetime, end_time: data[:date].to_datetime, title: data[:title])
-          data[:attendances].each do |user_id|
-            event.attendances.create(user_id: user_id)
-          end
-        end
-      end
-
-    end
-  end
-
-  def email_required?
-    false
-  end
-
-  def email_changed?
-    false
-  end
-
   def admin?(current_tenant)
     if super_admin
       true
@@ -170,15 +53,11 @@ class User < ActiveRecord::Base
   end
 
   def eligible_for_membership?
-    if attendances.joins(:event).where(events: {event_type: 'orientation' }).count >= 1 && attendances.joins(:event).where(events: {event_type: 'general_body_meeting' }).count >= 2 && attendances.joins(:event).where(events: {event_type: 'public_event' }).count >= 1
-      return true
-    else
-      return false
-    end
+    attendance_requirement('orientation', 1) and attendance_requirement('general_body_meeting', 2) and attendance_requirement('public_event', 1)
   end
 
   def pending_invoices?
-    Billing::Invoice.list_invoices(customer_id).select{|result| result.invoice.status == "pending"}.count > 0
+    ChargeBee::Invoice.invoices_for_customer(customer_id).select{|result| result.invoice.status == "pending"}.count > 0
   end
 
   def begin_enrollment
@@ -186,47 +65,36 @@ class User < ActiveRecord::Base
     twilio.send("Welcome, you have completed the attendance requirements for BYP100 membership, please pay your dues at www.byp100.org", phone)
   end
 
-   def subscribed?(plan = nil)
-    if plan == nil
-      if customer_id.present?
-        result = ChargeBee::Subscription.retrieve(customer_id)
-        subscription = result.subscription
-        if subscription.status != "cancelled" && subscription.status != "future" && subscription.status != "non_renewing"
-          true
-        else
-         false
-        end
-      else
-        false
-      end
-    else
-      aasm_state == plan
-    end
+   def subscribed?
+     ChargeBee::Subscription.retrieve(customer_id).subscription.status == "active" if customer_id.present?
    end
 
   aasm do
-    state :prospective, :initial => true
-    
-    state :active 
-
+    state :prospective, initial: true
+    state :active
     state :inactive
-
     state :alumni
 
     event :induct do
-      transitions :from => [:prospective], :to => :active
+      transitions from: [:prospective], to: :active
     end
 
     event :deactivate do
-      transitions :from => [:active], :to => :inactive
+      transitions from: [:active], to: :inactive
     end
 
     event :activate do
-      transitions :from => [:inactive], :to => :active
+      transitions from: [:inactive], to: :active
     end
 
     event :graduate do
-      transitions :from => [:active], :to => :alumni
+      transitions from: [:active], to: :alumni
     end
+  end
+
+  private
+
+  def attendance_requirement event_type, count
+    attendances.joins(:event).where(events: {event_type: event_type}).count >= count
   end
 end
