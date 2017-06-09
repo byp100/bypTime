18f4823eb406082287a8c219d8639f3450618dd1
Ayanga Okpokowuruk <ayanga@aok-solutions.com>
2017-06-09 03:41:16 -0400

    use case statement to always render json for webhook response

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -17,17 +17,21 @@ class WebhooksController < ApplicationController
   def chargebee_event
     puts "Processing #{params[:event_type]} event"
 
-    if params[:event_type] == 'invoice_created'
+    case params[:event_type]
+    when 'invoice_created'
       invoice = params[:content][:invoice]
       customer = User.find_by(customer_id: invoice[:customer_id])
 
-      unless customer.nil? or customer.manual_invoicing or invoice[:status] == 'paid'
-        result = ChargeBee::Invoice.collect_payment(invoice[:id])
-        render json: {outcome: 'Event Processed.', result: result}
-      end
+      result = ChargeBee::Invoice.collect_payment(invoice[:id]) unless customer.nil? or customer.manual_invoicing or paid invoice
+      render json: {outcome: 'Event Processed.', result: result}
     else
       render json: {outcome: 'Not processed.'}
     end
   end
 
+  private
+
+  def paid invoice
+    invoice[:status] == 'paid'
+  end
 end
