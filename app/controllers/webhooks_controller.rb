ce13bd856f8cd64f2ff11509065c7c093ca51eb3
Ayanga Okpokowuruk <ayanga@aok-solutions.com>
2017-06-09 03:09:45 -0400

    webhook should only attempt to collect invoice if payment_due/not_paid

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,18 +1,33 @@
 class WebhooksController < ApplicationController
-	protect_from_forgery with: :null_session
-	http_basic_authenticate_with name: ENV['webhooks_name'], password: ENV['webhooks_password']
+  protect_from_forgery with: :null_session
+  http_basic_authenticate_with name: ENV['webhooks_name'], password: ENV['webhooks_password']
 
-	def chargebee_event
-		if params[:event_type] == 'subscription_cancelled'
-		elsif params[:event_type] == 'invoice_created'
-			invoice = params[:content][:invoice]
-			customer = User.find_by(customer_id: invoice[:customer_id])
+  rescue_from ChargeBee::PaymentError do |ex|
+    render json: {ex: ex}, status: :unprocessable_entity
+  end
 
-			ChargeBee::Invoice.collect_payment(invoice[:id]) unless customer.manual_invoicing
-			render json: {outcome: 'Event Processed.'}
-		else
-			render json: {outcome: 'Not processed.'}
-		end
-	end
+  rescue_from ChargeBee::InvalidRequestError do |ex|
+    render json: {ex: ex}, status: :unprocessable_entity
+  end
+
+  rescue_from ChargeBee::APIError do |ex|
+    render json: {ex: ex}, status: :unprocessable_entity
+  end
+
+  def chargebee_event
+    puts "Processing #{params[:event_type]} event"
+
+    if params[:event_type] == 'invoice_created'
+      invoice = params[:content][:invoice]
+      customer = User.find_by(customer_id: invoice[:customer_id])
+
+      unless customer.nil? or customer.manual_invoicing or invoice[:status] == 'paid'
+        result = ChargeBee::Invoice.collect_payment(invoice[:id])
+        render json: {outcome: 'Event Processed.', result: result}
+      end
+    else
+      render json: {outcome: 'Not processed.'}
+    end
+  end
 
 end
